7620a7e3140ed628a7fc985356d67a14408cb32f
David BREYAULT <davidbreyault@gmail.com>
2022-05-02 20:56:28 +0200

    [TP1] Spring Initializer

---
+++ src/main/java/fr/david/ecommerce/TpECommerceApplication.java
@@ -0,0 +1,13 @@
+package fr.david.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/david/ecommerce/TpECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.david.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
