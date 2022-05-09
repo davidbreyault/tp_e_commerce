c98034c5341890353bb40a328872583a7fc70e0a
David BREYAULT <davidbreyault@gmail.com>
2022-05-09 22:46:15 +0200

    [TP2] Metadata : fichier de configuration

--- src/main/java/fr/david/ecommerce/TpECommerceApplication.java
+++ src/main/java/fr/david/ecommerce/TpECommerceApplication.java
@@ -1,14 +1,9 @@
 package fr.david.ecommerce;
 
-import fr.david.ecommerce.model.Client;
-import fr.david.ecommerce.model.Order;
-import fr.david.ecommerce.model.OrderProduct;
 import fr.david.ecommerce.model.Product;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
+import fr.david.ecommerce.service.ProductService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //@SpringBootApplication
 public class TpECommerceApplication {
@@ -16,18 +11,17 @@ public class TpECommerceApplication {
     public static void main(String[] args) {
         // SpringApplication.run(CdaECommerceApplication.class, args);
 
-        Product product1 = new Product(1l, "Sandwich 1", "Desc du sandwich 1", 7d, "url1", 3);
-        Product product2 = new Product(2l, "Sandwich 2", "Desc du sandwich 2", 8d, "url2", 4);
-        Product product3 = new Product(3l, "Sandwich 3", "Desc du sandwich 3", 12d, "url3", 54);
+        ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
+        // Récupération de l'instance de type ProductService créé par le bean
+        ProductService productService = context.getBean("products", ProductService.class);
 
-        Client client = new Client(1l, "David", "Toto37");
+        Product product1 = new Product(1l, "Produit 1", "Description du produit 1", 12d, "url1", 1);
+        Product product2 = new Product(2l, "Produit 2", "Description du produit 2", 32d, "url2", 4);
+        Product product3 = new Product(3l, "Produit 3", "Description du produit 3", 22d, "url3", 54);
+        productService.save(product1);
+        productService.save(product2);
+        productService.save(product3);
 
-        Order order = new Order(1L, LocalDate.now(), null, client, new ArrayList<OrderProduct>());
-
-        order.addProduct(product1, 2);
-        order.addProduct(product2, 1);
-        order.setStatus("Payée");
-
-        System.out.println(order);
+        productService.getAllProducts().stream().forEach(System.out::println);
     }
 }
