ae38a3de776d011df92bdb988fffaeef17ce09dc
David Breyault <72495222+davidbreyault@users.noreply.github.com>
2022-05-23 22:30:33 +0200

    [TP4] Spring web 1 (#4)
    * [TP4] Spring Boot
    
    * [TP4] La vue
    
    * [TP4] Landing page

--- src/main/java/fr/david/ecommerce/TpECommerceApplication.java
+++ src/main/java/fr/david/ecommerce/TpECommerceApplication.java
@@ -1,55 +1,15 @@
 package fr.david.ecommerce;
 
-import fr.david.ecommerce.exception.StockException;
-import fr.david.ecommerce.model.Client;
-import fr.david.ecommerce.model.Order;
-import fr.david.ecommerce.model.OrderProduct;
-import fr.david.ecommerce.model.Product;
-import fr.david.ecommerce.service.ClientService;
-import fr.david.ecommerce.service.OrderService;
-import fr.david.ecommerce.service.ProductService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-//@SpringBootApplication
 @ComponentScan("fr.*")
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TpECommerceApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TpECommerceApplication.class);
-
-        ProductService productService = context.getBean("products", ProductService.class);
-        ClientService clientService = context.getBean("clients", ClientService.class);
-        OrderService orderService = context.getBean("orders", OrderService.class);
-
-        Product product1 = new Product(1l, "Produit 1", "Description du produit 1", 12d, "url1", 1);
-        Product product2 = new Product(2l, "Produit 2", "Description du produit 2", 32d, "url2", 4);
-        Product product3 = new Product(3l, "Produit 3", "Description du produit 3", 22d, "url3", 54);
-        productService.save(product1);
-        productService.save(product2);
-        productService.save(product3);
-
-        Client client1 = new Client(1l, "John_Doe", "helloworld");
-        Client client2 = new Client(2l, "Jon_Hopkins", "ilovepiano");
-        clientService.save(client1);
-        clientService.save(client2);
-
-        Order order = new Order(1l, LocalDate.now(), null, client1, new ArrayList<OrderProduct>());
-        order.addProduct(product1, 2);
-        order.addProduct(product3, 4);
-
-        orderService.create(order);
-        System.out.println(order);
-
-        try {
-            orderService.update(order);
-        } catch (StockException e) {
-            e.printStackTrace();
-        }
-        // Vérifiez qu’une commande ne passe pas à “Payée” si un produit n’est pas en quantité suffisante.
-        System.out.println(order);
+        SpringApplication.run(TpECommerceApplication.class, args);
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/parts/header.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/styles.css">
+    <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <title>Shop sandwich</title>
+</head>
+
+<body>
+<header>
+    <div class="container">
+        <nav class="navbar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sandwiches</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+            <span class="material-symbols-outlined">shopping_cart</span>
+        </nav>
+    </div>
+</header>
